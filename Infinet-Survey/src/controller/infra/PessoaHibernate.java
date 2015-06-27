242a3f400a60d8c10b0583b84fe87cebabf91a70
tazio <tazio.fernandes@gmail.com>
2015-06-27 18:05:59 -0300

    Feito a primeira classe do package infra que obtem a sessão para  tratar as solicitações para o banco

---
+++ Infinet-Survey/src/controller/infra/PessoaHibernate.java
@@ -0,0 +1,41 @@
+package controller.infra;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+import controller.repositorio.Modulo;
+import controller.repositorio.Pessoa;
+
+public class PessoaHibernate implements Pessoa {
+	
+	private Session session;
+	
+	
+
+	public PessoaHibernate(Session session) {
+		super();
+		this.session = session;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Pessoa> todos() {
+		
+		return session.createCriteria(Pessoa.class).addOrder(Order.asc("id")).list();
+	}
+
+	@Override
+	public Pessoa guardar(Pessoa pessoa) {
+		
+		return (Pessoa) session.merge(pessoa);
+	}
+
+	@Override
+	public void remover(Pessoa pessoa) {
+		this.session.delete(pessoa);
+
+	}
+
+}
--- Infinet-Survey/src/controller/repositorio/Pessoa.java
+++ Infinet-Survey/src/controller/repositorio/Pessoa.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface Pessoa {
 	
 	public List<Pessoa> todos();
-	public Modulo  guardar(Pessoa pessoa);
+	public Pessoa  guardar(Pessoa pessoa);
 	public void remover( Pessoa pessoa);
 
 
